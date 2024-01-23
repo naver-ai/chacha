dfc4027e9341af5f522d1ac88a0c376bc3f06176
Young-Ho Kim <yghokim@gmail.com>
2024-01-24 00:22:42 +0900

    Added author home pages.

--- app/sections/Members.tsx
+++ app/sections/Members.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { Section } from "../components/Section"
+import { HomeIcon } from '@heroicons/react/20/solid'
 
 const members = [
     {
@@ -7,6 +8,7 @@ const members = [
         affiliation: "Univ. of Michigan, Ann Arbor",
         role: "PhD candidate",
         thumb: "/images/thumb-wsseo.jpg",
+        web: "https://seowoosuk.wixsite.com/home",
         note: "*Research intern at NAVER AI Lab"
     },
     {
@@ -20,18 +22,21 @@ const members = [
         affiliation: "Univ. of Michigan, Ann Arbor",
         role: "Associate Professor",
         thumb: "/images/thumb-sypark.jpg",
+        web: "http://sunyoungpark.weebly.com/",
     },
     {
         name: "Mark S. Ackerman",
         affiliation: "Univ. of Michigan, Ann Arbor",
         role: "Professor",
         thumb: "/images/thumb-mackerman.jpg",
+        web: "https://www.socialworldsresearch.org/si/ackerm"
     },
     {
         name: "Young-Ho Kim",
         affiliation: "NAVER AI Lab",
         role: "Research Scientist",
         thumb: "/images/thumb-yhkim.jpg",
+        web: "http://younghokim.net"
     }
 ]
 
@@ -45,7 +50,13 @@ export const Members = () => {
                         <Image alt={`Thumbnail of ${member.name}`} src={member.thumb} fill={true}/>
                     </div>
                     <div className="flex-1 ml-4">
-                        <div className="text-xl font-semibold">{member.name}</div>
+                        <div className="text-xl font-semibold flex items-center">
+                            <span>{member.name}</span>
+                            {
+                                member.web != null ? <a href={member.web} target="_blank"><HomeIcon className="w-5 h-5 text-gray-500 mb-0.5 ml-1"/></a> : null
+                            }
+                        </div>
+                        
                         <div className="font-light">{member.role}</div>
                         <div className="font-light">{member.affiliation}</div>
                         {member.note != null ? <div className="font-light italic text-sm text-orange-500">{member.note}</div> : null }
@@ -56,8 +67,8 @@ export const Members = () => {
         </div>
         <div className="mt-8 flex items-center gap-6">
             <Image className="w-[200px]" alt="NAVER logo" src={require("../../public/images/logos/ai_lab_logo_vertical.png")} width={1728} height={552}/>
-            <Image className="w-20" alt="UMSI logo" src={require("../../public/images/logos/umsi-logo.svg")} unoptimized={true}/>
-            <Image className="w-[250px]" alt="Wonkwang logo" src={require("../../public/images/logos/wonkwang-logo.svg")} unoptimized={true}/>
+            <a href="https://www.si.umich.edu/" target="_blank"><Image className="w-20" alt="UMSI logo" src={require("../../public/images/logos/umsi-logo.svg")} unoptimized={true}/></a>
+            <a href="https://global.wkuh.org/" target="_blank"><Image className="w-[250px]" alt="Wonkwang logo" src={require("../../public/images/logos/wonkwang-logo.svg")} unoptimized={true}/></a>
             
         </div>
     </Section>
