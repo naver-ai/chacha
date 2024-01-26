98f411f12bc3a822c91337eb4c45aec120688984
Young-Ho Kim <yghokim@gmail.com>
2024-01-27 01:26:23 +0900

    Added preprint pdf.

--- app/sections/Abstract.tsx
+++ app/sections/Abstract.tsx
@@ -8,7 +8,7 @@ export const Abstract = () => {
         Children typically learn to identify and express emotions through sharing their stories and feelings with others, particularly their family. However, it is challenging for parents or siblings to have emotional communication with children since children are still developing their communication skills. We present <b>ChaCha</b>, a chatbot that encourages and guides children to share personal events and associated emotions. <strong>ChaCha combines a state machine and large language models (LLMs) to keep the dialogue on track while carrying on free-form conversations.</strong> Through an exploratory study with 20 children (aged 8-12), we examine how ChaCha prompts children to share personal events and guides them to describe associated emotions. Participants perceived ChaCha as a close friend and shared their stories on various topics, such as family trips and personal achievements. Based on the findings, we discuss opportunities for leveraging LLMs to design child-friendly chatbots to support children in sharing emotions.
         </p>
         <div className="flex flex-wrap gap-4 mt-4">
-            <a className="icon-label-button bg-teal-500 pl-4" aria-disabled={true} href="./public/chacha_preprint_230127.pdf" target="_blank">
+            <a className="icon-label-button bg-teal-500 pl-4" aria-disabled={true} href={"./public/chacha_preprint_230127.pdf"} target="_blank">
                 <NewspaperIcon className="w-6 h-6 text-white"/>
                 <span>Paper</span>
             </a>
